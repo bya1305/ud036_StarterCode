4b5bb60fde54ddec09651f00a87c2cb5400a9e54
Josh <josh.avina78@gmail.com>
2018-05-21 18:55:51 -0700

    added movie class with initializer

---
+++ movie.py
@@ -0,0 +1,5 @@
+class Movie():
+    def __init__(self, movie_title, poster, trailer):
+        self.title = movie_title
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer
