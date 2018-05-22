2cfea156edbf5829e237180ac0f533d904b4ef43
Josh <josh.avina78@gmail.com>
2018-05-21 19:21:01 -0700

    added movie list with Movie instances

---
+++ run_program.py
@@ -0,0 +1,11 @@
+import movie
+
+darkest_hour = movie.Movie("Darkest Hour", "https://ia.media-imdb.com/images/M/MV5BNTU4MjMwOTgyMV5BMl5BanBnXkFtZTgwODQzNjY2NDM@._V1_SX300.jpg", "https://www.youtube.com/watch?v=LtJ60u7SUSw")
+thor_r = movie.Movie("Thor: Ragnarok", "https://ia.media-imdb.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SX300.jpg", "https://www.youtube.com/watch?v=v7MGUNV8MxU")
+justice_league = movie.Movie("Justice League", "https://ia.media-imdb.com/images/M/MV5BYWVhZjZkYTItOGIwYS00NmRkLWJlYjctMWM0ZjFmMDU4ZjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg", "https://www.youtube.com/watch?v=r9-DM9uBtVI")
+skyfall = movie.Movie("Skyfall", "https://ia.media-imdb.com/images/M/MV5BNDVhZmJiYWMtNmIzMC00ZWNiLTkzZDYtNGNlZmViMGM4OGExXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SY1000_CR0,0,674,1000_AL_.jpg", "https://www.youtube.com/watch?v=6kw1UVovByw")
+dark_knight = movie.Movie("The Dark Knight", "https://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg", "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+dark_knight_rises = movie.Movie("The Dark Knight Rises", "https://ia.media-imdb.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg", "https://www.youtube.com/watch?v=GokKUqLcvD8")
+ready_player_one = movie.Movie("Ready Player One", "https://ia.media-imdb.com/images/M/MV5BY2JiYTNmZTctYTQ1OC00YjU4LWEwMjYtZjkwY2Y5MDI0OTU3XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_SY1000_CR0,0,674,1000_AL_.jpg", "https://www.youtube.com/watch?v=cSp1dM2Vj48")
+
+movie_list = [darkest_hour, thor_r, justice_league, skyfall, dark_knight, dark_knight_rises, ready_player_one]
