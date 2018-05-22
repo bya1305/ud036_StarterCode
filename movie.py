57141e8ffb875b737c447b60fa232808ad704538
Josh <josh.avina78@gmail.com>
2018-05-22 09:35:52 -0700

    style guide changes

--- movie.py
+++ movie.py
@@ -1,4 +1,5 @@
 class Movie():
+    """Creates a Movie instance with basic info about individual movie"""
     def __init__(self, movie_title, poster, trailer):
         self.title = movie_title
         self.poster_image_url = poster
